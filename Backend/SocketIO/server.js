f6740d2ee9d1f5712003b78493fcb12bb7286102
Afaiz8720 <96206044+Afaiz8720@users.noreply.github.com>
2024-10-13 14:02:02 +0530

    updated backend url

--- Backend/SocketIO/server.js
+++ Backend/SocketIO/server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3001",
+    origin: ["https://message-app-6.onrender.com"],
     methods: ["GET", "POST"],
   },
 });
